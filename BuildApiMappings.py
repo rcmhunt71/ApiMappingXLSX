b206baceba9c0376d8e21642f61edbb3ec139d4e
Christopher <rcmhunt71@gmail.com>
2020-05-02 17:08:44 -0500

    Refactored to define Excel class first, utility routines second. * Compared to listing the class second, so it is "buried" in the file

--- BuildApiMappings.py
+++ BuildApiMappings.py
@@ -30,81 +30,11 @@ RESULT_TYPE = "ResultType"
 VERSION = "Version"
 
 
-def build_version_str(version_info: typing.Dict[str, str]) -> str:
-    """
-    Builds the version string from the API response data (dict)
-    Args:
-        version_info: dictionary of major, minor, hotfix, and build elements.
-
-    Returns:
-        str: <MAJOR>.<MINOR>.<HOT_FIX>.<BUILD>
-    """
-    version_elements = [MAJOR, MINOR, HOT_FIX, BUILD]
-    return ".".join([str(version_info.get(elem)) for elem in version_elements])
-
-
-def get_api_list(url: str) -> typing.Dict[str, typing.Any]:
-    """
-    Call the specified API and return the response payload as JSON.
-
-    Returns: (dict) JSON response, if the response code was 2xx.
-
-    """
-    response = requests.get(url)
-    if int(response.status_code) / 100 != 2:
-        print(f"Unable to get API information. Rec'd status code: {response.status_code}")
-        return {}
-
-    return json.loads(response.text)
-
-
-def verify_cols_are_present(source: typing.List[str], expected: typing.List[str]) -> bool:
-    """
-    Verify the expected data columns were returned in the response.
-    Args:
-        source: List of columns returned in the response data
-        expected: List of expected/required columns
-
-    Returns:
-        Boolean: True = Columns present, False = Actual columns list does not match the expected list (case sensitive).
-
-    """
-    diff = set(expected) - set(source)
-    if diff:
-        print(f"ERROR:\n\tColumn Mismatch(es): {diff}")
-    return not diff
-
-
-def write_csv_from_dict(
-        filename: str, ordered_column_header: typing.List[str], data_list: typing.List[dict],
-        sort_key=NAME) -> typing.NoReturn:
-    """
-    Writes the dictionary for each API in the list to a CSV file, using the expected column headers in the
-    provided order.
-
-    Args:
-        filename: Name of file to write
-        ordered_column_header: list of columns to write to the CSV file.
-        data_list: list where each element defines a dictionary describing an API
-        sort_key: (optional) sort the rows based on one of the header elements (DEFAULT: NAME Constant)
-
-    Returns:
-        None.
-    """
-
-    # Open the file, write the header row, and then the data.
-    with open(filename, "w", newline='') as FILE:
-        writer = csv.writer(FILE)
-        writer.writerow(ordered_column_header)
-
-        # Sort the data by the sort_key and write to file.
-        for row in sorted(data_list, key=operator.itemgetter(sort_key)):
-            writer.writerow([row.get(col) for col in ordered_column_header])
-
-    print(f"Wrote file: {filename}")
-
-
+# -------------------------------------------------------
+#                  CLASSES
+# -------------------------------------------------------
 class ExcelFile:
+    """ Used to build and populate an Excel file. (Currently implementation-specific to this application) """
 
     LEFT = "left"
     CENTER = "center"
@@ -264,9 +194,86 @@ class ExcelFile:
         print(f"Created and populated an excel worksheet: {unique_worksheet_name}")
 
 
-# +------------------------------------+
-# |           APPLICATION              |
-# +------------------------------------+
+# -------------------------------------------------------
+#             UTILITY-SPECIFIC ROUTINES
+# -------------------------------------------------------
+def build_version_str(version_info: typing.Dict[str, str]) -> str:
+    """
+    Builds the version string from the API response data (dict)
+    Args:
+        version_info: dictionary of major, minor, hotfix, and build elements.
+
+    Returns:
+        str: <MAJOR>.<MINOR>.<HOT_FIX>.<BUILD>
+    """
+    version_elements = [MAJOR, MINOR, HOT_FIX, BUILD]
+    return ".".join([str(version_info.get(elem)) for elem in version_elements])
+
+
+def get_api_list(url: str) -> typing.Dict[str, typing.Any]:
+    """
+    Call the specified API and return the response payload as JSON.
+
+    Returns: (dict) JSON response, if the response code was 2xx.
+
+    """
+    response = requests.get(url)
+    if int(response.status_code) / 100 != 2:
+        print(f"Unable to get API information. Rec'd status code: {response.status_code}")
+        return {}
+
+    return json.loads(response.text)
+
+
+def verify_cols_are_present(source: typing.List[str], expected: typing.List[str]) -> bool:
+    """
+    Verify the expected data columns were returned in the response.
+    Args:
+        source: List of columns returned in the response data
+        expected: List of expected/required columns
+
+    Returns:
+        Boolean: True = Columns present, False = Actual columns list does not match the expected list (case sensitive).
+
+    """
+    diff = set(expected) - set(source)
+    if diff:
+        print(f"ERROR:\n\tColumn Mismatch(es): {diff}")
+    return not diff
+
+
+def write_csv_from_dict(
+        filename: str, ordered_column_header: typing.List[str], data_list: typing.List[dict],
+        sort_key=NAME) -> typing.NoReturn:
+    """
+    Writes the dictionary for each API in the list to a CSV file, using the expected column headers in the
+    provided order.
+
+    Args:
+        filename: Name of file to write
+        ordered_column_header: list of columns to write to the CSV file.
+        data_list: list where each element defines a dictionary describing an API
+        sort_key: (optional) sort the rows based on one of the header elements (DEFAULT: NAME Constant)
+
+    Returns:
+        None.
+    """
+
+    # Open the file, write the header row, and then the data.
+    with open(filename, "w", newline='') as FILE:
+        writer = csv.writer(FILE)
+        writer.writerow(ordered_column_header)
+
+        # Sort the data by the sort_key and write to file.
+        for row in sorted(data_list, key=operator.itemgetter(sort_key)):
+            writer.writerow([row.get(col) for col in ordered_column_header])
+
+    print(f"Wrote file: {filename}")
+
+
+# -------------------------------------------------------
+#           APPLICATION/EXECUTABLE
+# -------------------------------------------------------
 if __name__ == "__main__":
 
     # API URL for list of all APIs
@@ -303,7 +310,6 @@ if __name__ == "__main__":
             expected=list(column_alignment.keys())):
         print("Unable to generate files, data columns do not match expected columns...\nExiting.\n")
         sys.exit()
-
     print("All expected columns present, generating designated files.")
 
     # If XLSX, create the workbook and set the properties.
@@ -335,4 +341,4 @@ if __name__ == "__main__":
         xlsx_workbook.close_workbook()
 
     # Print out the version for reference.
-    print(f"Files generated for API VERSION: {version}")
+    print(f"Files generated for PRICE API VERSION: {version}")
